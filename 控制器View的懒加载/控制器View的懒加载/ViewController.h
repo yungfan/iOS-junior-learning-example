3c99904861ac0568efd03448863d63b5bc9d1987
杨帆 <yangfanvw@qq.com>
2018-11-14 20:58:35 +0800

    新增控制器View的懒加载

---
+++ 控制器View的懒加载/控制器View的懒加载/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  控制器View的懒加载
+//
+//  Created by stu1 on 2018/11/14.
+//  Copyright © 2018年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 控制器View的懒加载/控制器View的懒加载/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  控制器View的懒加载
+//
+//  Created by stu1 on 2018/11/14.
+//  Copyright © 2018年 abc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+    NSLog(@"viewDidLoad");
+}
+
+
+@end
