4f08f8244a0eb1d145d9bf399d6cc8f5117b6c8b
杨帆 <yangfanvw@qq.com>
2018-12-21 16:40:24 +0800

    更新UIView动画

--- UIView动画/UIView动画/ViewController.m
+++ UIView动画/UIView动画/ViewController.m
@@ -2,15 +2,27 @@
 //  ViewController.m
 //  UIView动画
 //
-//  Created by teacher on 16/12/14.
-//  Copyright (c) 2016年 安徽商贸职业技术学院. All rights reserved.
-//
+//  Created by stu1 on 2018/12/21.
+//  Copyright © 2018年 abc. All rights reserved.
+//  动画：1.UIView动画  2.核心动画（Core Animation、Layer）
+
+//支持的属性
+//@property frame
+//@property bounds
+//@property center
+//@property transform
+//@property alpha
+//@property backgroundColor
+//@property contentStretch
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
-@property(nonatomic, strong) UIImageView *imgView;
+@property (strong, nonatomic) UIView *redView;
+@property (strong, nonatomic) UIView *orangeView;
+@property (strong, nonatomic) UIView *blueView;
+@property (strong, nonatomic) UIView *greedView;
 
 @end
 
@@ -20,54 +32,72 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    self.redView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
+    self.redView.backgroundColor =  [UIColor redColor];
+    [self.view addSubview:self.redView];
     
-    UIImage *img = [UIImage imageNamed:@"leaf"];
+    self.orangeView = [[UIView alloc]initWithFrame:CGRectMake(0, 150, 50, 50)];
+    self.orangeView.backgroundColor =  [UIColor orangeColor];
+    [self.view addSubview:self.orangeView];
     
-    self.imgView = [[UIImageView alloc]initWithFrame:CGRectMake(100, 100, 42, 38)];
+    self.blueView = [[UIView alloc]initWithFrame:CGRectMake(0, 300, 50, 50)];
+    self.blueView.backgroundColor =  [UIColor blueColor];
+    [self.view addSubview:self.blueView];
     
-    self.imgView.image = img;
-    
-    
-    [self.view addSubview:self.imgView];
-    
+    self.greedView = [[UIView alloc]initWithFrame:CGRectMake(0, 450, 50, 50)];
+    self.greedView.backgroundColor =  [UIColor greenColor];
+    [self.view addSubview:self.greedView];
 }
 
-/**
- *  触摸屏幕的时候调用
- *
- *  @param touches <#touches description#>
- *  @param event   <#event description#>
- */
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    
+    //1. 最简单的动画 第一个参数：动画时间 第二个Block就是写动画的最终效果 中间的过渡由系统自动完成
+    [UIView animateWithDuration:5.0 animations:^{
+
+    }];
+    
+    //2. 多一个参数 动画完成之后的操作
+//    [UIView animateWithDuration:3.0 animations:^{
+//        self.redView.frame = CGRectMake(200, 200, 100, 100);
+//        self.redView.backgroundColor = [UIColor blueColor];
+//        self.redView.alpha = 0.5;
+//    } completion:^(BOOL finished) {
+//
+//        NSLog(@"动画完成");
+//    }];
 
     
-   UITouch *touch =  [touches anyObject];
+    //3. 多两个参数：delay（延迟）、options（动画选项）
+    //对比四种选项（说白了就是动画执行的快慢）
+    //UIViewAnimationOptionCurveEaseInOut //时间曲线函数，由慢到快
+    //UIViewAnimationOptionCurveEaseIn //时间曲线函数，由慢到特别快
+    //UIViewAnimationOptionCurveEaseOut //时间曲线函数，由快到慢
+    //UIViewAnimationOptionCurveLinear //时间曲线函数，匀速
+    [UIView animateWithDuration:5.0 delay:1.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        
+        self.redView.frame = CGRectMake(400, 0, 50, 50);
+        
+    } completion:nil];
     
-   CGPoint point =  [touch locationInView:self.view];
+    [UIView animateWithDuration:5.0 delay:1.0 options:UIViewAnimationOptionCurveLinear animations:^{
+        
+        self.orangeView.frame = CGRectMake(400, 150, 50, 50);
+        
+    } completion:nil];
     
+    [UIView animateWithDuration:5.0 delay:1.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        
+        self.blueView.frame = CGRectMake(400, 300, 50, 50);
+        
+    } completion:nil];
     
-   //UIView动画基本框架 第一种Block方式
-//   [UIView animateWithDuration:3.0 animations:^{
-//       
-//       //动画要执行的动作
-//       self.imgView.center = point;
-//       
-//   }];
-    
-    
-    //第二种
-    [UIView beginAnimations:@"abc" context:nil];
-    [UIView setAnimationDuration:1.0];
-    self.imgView.center = point;
-    [UIView commitAnimations];
-    
-
-
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [UIView animateWithDuration:5.0 delay:1.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        
+        self.greedView.frame = CGRectMake(400, 450, 50, 50);
+        
+    } completion:nil];
 }
 
 @end
--- UIView动画/UIView动画/main.m
+++ UIView动画/UIView动画/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  UIView动画
 //
-//  Created by teacher on 16/12/14.
-//  Copyright (c) 2016年 安徽商贸职业技术学院. All rights reserved.
+//  Created by stu1 on 2018/12/21.
+//  Copyright © 2018年 abc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
